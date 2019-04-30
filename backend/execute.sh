3c0e256856c645b873a40af425581629841f5031
Edmund <e32wong@uwaterloo.ca>
2019-04-30 13:28:35 -0400

    - fallback to jdk 8

--- backend/execute.sh
+++ backend/execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
   
-java -cp target/my-app-1.0-SNAPSHOT-jar-with-dependencies.jar com.mycompany.app.App ./config/fireemblem/
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp target/my-app-1.0-SNAPSHOT-jar-with-dependencies.jar com.mycompany.app.App ./config/fireemblem/
